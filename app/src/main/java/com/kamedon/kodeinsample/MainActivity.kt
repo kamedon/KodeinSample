664834f071a5ca7a2f70484016ce3d7e394e2825
kamedon <hkamei@gmail.com>
2017-06-17 15:54:08 +0900

    Databindingを導入

--- app/src/main/java/com/kamedon/kodeinsample/MainActivity.kt
+++ app/src/main/java/com/kamedon/kodeinsample/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.kamedon.kodeinsample
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.kamedon.kodeinsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }
