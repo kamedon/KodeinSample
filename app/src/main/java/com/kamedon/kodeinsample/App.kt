42c4eeabce00327bfa8819c3a89e12b680503b16
kamedon <hkamei@gmail.com>
2017-06-18 16:44:01 +0900

    kotlin 1.1.2-5

--- app/src/main/java/com/kamedon/kodeinsample/App.kt
+++ app/src/main/java/com/kamedon/kodeinsample/App.kt
@@ -2,10 +2,7 @@ package com.kamedon.kodeinsample
 
 import android.app.Application
 import android.content.Context
-import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.KodeinAware
-import com.github.salomonbrys.kodein.bind
-import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.*
 import com.kamedon.kodeinsample.di.NetworkModule
 import com.kamedon.kodeinsample.di.PresentationModule
 
@@ -15,8 +12,8 @@ import com.kamedon.kodeinsample.di.PresentationModule
 
 class App : Application(), KodeinAware {
     override val kodein: Kodein = Kodein {
+        bind<Context>() with instance(this@App)
         import(NetworkModule.module)
         import(PresentationModule.module)
     }
-
 }
